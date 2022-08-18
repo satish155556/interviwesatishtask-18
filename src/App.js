3baf0571f45cc0b1483eb2feae37270eb301d548
mukthashankar <82582739+mukthashankar@users.noreply.github.com>
2022-08-18 14:30:16 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  let [input,setInput] = useState('');
+  let [result,setResult] = useState();
+
+  let hanlder = s  =>{
+      setInput(s.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='cal'>
+      <h1>Calculator</h1>
+      <input type='text' placeholder='0' value={input} name='input' onChange={hanlder}></input>
+      <br/>
+      <h2>Result : {result}</h2>
+      <button onClick={()=> setInput('')}>C</button>
+      <button onClick={()=> setInput(input+9)}>9</button>
+      <button onClick={()=> setInput(input+8)}>8</button>
+      <button onClick={()=> setInput(input+7)}>7</button>
+      <button onClick={()=> setInput(input+6)}>6</button>
+      <button onClick={()=> setInput(input+5)}>5</button>
+      <button onClick={()=> setInput(input+4)}>4</button>
+      <button onClick={()=> setInput(input+3)}>3</button>
+      <button onClick={()=> setInput(input+2)}>2</button>
+      <button onClick={()=> setInput(input+1)}>1</button>
+      <button onClick={()=> setInput(input+'+')}>+</button>
+      <button onClick={()=> setInput(input+'-')}>-</button>
+      <button onClick={()=> setInput(input+'*')}>*</button>
+      <button onClick={()=> setInput(input+'/')}>/</button>
+      <button onClick={()=> setInput(input+'%')}>%</button>
+      <button className='clear' onClick={()=> setInput('')}>Clear</button>
+      <button className='total' onClick={()=>{ setResult (eval(input))}}>=</button>
+      </div>
+      </div>
+   
   );
 }
 
